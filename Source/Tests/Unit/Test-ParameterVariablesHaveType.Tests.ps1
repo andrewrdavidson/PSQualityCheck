60d98a7b886eb10282c40b02ccbf726f02c8e72e
Andrew Davidson <github@kelrys.com>
2021-01-02 18:18:08 +0000

    Extra parameter checks and finished function tests

--- Source/Tests/Unit/Test-ParameterVariablesHaveType.Tests.ps1
+++ Source/Tests/Unit/Test-ParameterVariablesHaveType.Tests.ps1
@@ -18,11 +18,49 @@ Describe "Test-ParameterVariablesHaveType.Tests" {
 
             }
 
+            It "should $parameter not belong to a parameter set" -TestCases @{ 'parameter' = $parameter } {
+
+                (Get-Command -Name 'Test-ParameterVariablesHaveType').Parameters[$parameter].ParameterSets.Keys | Should -Be '__AllParameterSets'
+
+            }
+
+        }
+
+        It "should ParameterVariables type be HashTable" {
+
+            (Get-Command -Name 'Test-ParameterVariablesHaveType').Parameters['ParameterVariables'].ParameterType.Name | Should -Be 'hashtable'
+
         }
 
     }
 
     Context "Function tests" {
+
+        It "parameter ParameterWithType passes" {
+
+            {
+                $parameterVariable = @{
+                    'ParameterWithType' = '[string]'
+                }
+
+                Test-ParameterVariablesHaveType -ParameterVariables $parameterVariable
+
+            } | Should -Not -Throw
+
+        }
+
+        It "parameter ParameterWithoutType throws" {
+
+            {
+                $parameterVariable = @{
+                    'ParameterWithoutType' = $null
+                }
+
+                Test-ParameterVariablesHaveType -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
 
     }
 
