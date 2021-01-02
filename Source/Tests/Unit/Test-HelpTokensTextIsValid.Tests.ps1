a635bc2b8a897e923151d408acc88a011d3578a7
Andrew Davidson <github@kelrys.com>
2021-01-02 20:55:42 +0000

    Added function tests

--- Source/Tests/Unit/Test-HelpTokensTextIsValid.Tests.ps1
+++ Source/Tests/Unit/Test-HelpTokensTextIsValid.Tests.ps1
@@ -20,9 +20,166 @@ Describe "Test-HelpTokensTextIsValid.Tests" {
 
         }
 
+        It "should HelpTokens type be HashTable" -TestCases @{ 'parameter' = $parameter } {
+
+            (Get-Command -Name 'Test-HelpTokensTextIsValid').Parameters['HelpTokens'].ParameterType.Name | Should -Be 'HashTable'
+
+        }
+
     }
 
     Context "Function tests" {
+
+        It "should not throw when checking help element text where it exists" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+                                Convert the help comment into an object
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Not -Throw
+
+        }
+
+        It "should throw when checking help element text where it is empty" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when checking help element text where it is missing" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Throw
+
+        }
+
+        It "should not throw when checking multiple valid help element text values" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+                                The SYNOPSIS property
+                                .DESCRIPTION
+                                The DESCRIPTION property
+                                .PARAMETER Path
+                                The Path parameter
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Not -Throw
+
+        }
+
+        It "should throw when checking multiple help element text values where one is empty" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+
+                                .DESCRIPTION
+                                The DESCRIPTION property
+                                .PARAMETER Path
+                                The Path parameter
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when checking multiple help element text values where one is missing" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+                                .DESCRIPTION
+                                The DESCRIPTION property
+                                .PARAMETER Path
+                                The Path parameter
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when checking multiple help element text values where all are empty" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+
+                                .DESCRIPTION
+
+                                .PARAMETER Path
+
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when checking multiple help element text values where all are missing" {
+
+            {
+                $helpComment = "<#
+                                .SYNOPSIS
+                                .DESCRIPTION
+                                .PARAMETER Path
+                                #>"
+
+                #TODO Replace this with the correct tokens for the above help
+                $help = Convert-Help -HelpComment $helpComment
+
+                Test-HelpTokensTextIsValid -HelpTokens $help
+
+            } | Should -Throw
+
+        }
 
     }
 
