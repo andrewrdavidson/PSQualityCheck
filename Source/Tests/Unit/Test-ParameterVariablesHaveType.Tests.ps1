f343a510ade19a8afaaee7dee20c6c5592e600e9
Andrew Davidson <github@kelrys.com>
2021-01-03 12:31:21 +0000

    Comment tidy

--- Source/Tests/Unit/Test-ParameterVariablesHaveType.Tests.ps1
+++ Source/Tests/Unit/Test-ParameterVariablesHaveType.Tests.ps1
@@ -10,10 +10,7 @@ Describe "Test-ParameterVariablesHaveType.Tests" {
 
             It "should have $parameter as a mandatory parameter" -TestCases @{ 'parameter' = $parameter } {
 
-                # Check whether the parameter exists
                 (Get-Command -Name 'Test-ParameterVariablesHaveType').Parameters[$parameter].Name | Should -BeExactly $parameter
-
-                # Check whether or not it's mandatory
                 (Get-Command -Name 'Test-ParameterVariablesHaveType').Parameters[$parameter].Attributes.Mandatory | Should -BeTrue
 
             }
