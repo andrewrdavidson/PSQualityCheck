45006ebf1573050bf78ff49f1029482af927ddec
Andrew Davidson <github@kelrys.com>
2021-01-03 12:31:21 +0000

    Comment tidy

--- Source/Tests/Unit/Test-ImportModuleIsValid.Tests.ps1
+++ Source/Tests/Unit/Test-ImportModuleIsValid.Tests.ps1
@@ -11,10 +11,7 @@ Describe "Test-ImportModuleIsValid.Tests" {
 
             It "should have $parameter as a mandatory parameter" -TestCases @{ 'parameter' = $parameter } {
 
-                # Check whether the parameter exists
                 (Get-Command -Name 'Test-ImportModuleIsValid').Parameters[$parameter].Name | Should -BeExactly $parameter
-
-                # Check whether or not it's mandatory
                 (Get-Command -Name 'Test-ImportModuleIsValid').Parameters[$parameter].Attributes.Mandatory | Should -BeTrue
 
             }
