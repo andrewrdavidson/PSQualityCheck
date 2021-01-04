a4218946497f8fad09352de0fdd5c908ff4adac8
Andrew Davidson <github@kelrys.com>
2021-01-04 13:38:08 +0000

    Added function tests

--- Source/Tests/Unit/Test-HelpTokensParamsMatch.Tests.ps1
+++ Source/Tests/Unit/Test-HelpTokensParamsMatch.Tests.ps1
@@ -34,6 +34,233 @@ Describe "Test-HelpTokensParamsMatch.Tests" {
 
     Context "Function tests" {
 
+        It "should not throw when single param block variables matches .PARAMETER help when valid" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterWithType'
+                            "LineNumber" = 1
+                            "Text" = "ParameterWithType description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterWithType" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Not -Throw
+
+        }
+
+        It "should throw when single param block variable does not match .PARAMETER help when invalid" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterUnmatched'
+                            "LineNumber" = 1
+                            "Text" = "ParameterUnmatched description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterWithType" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
+        It "should not throw when multiple param block variables match .PARAMETER help when valid" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterOne'
+                            "LineNumber" = 1
+                            "Text" = "ParameterOne description"
+                        },
+                        @{
+                            "Name" = 'ParameterTwo'
+                            "LineNumber" = 1
+                            "Text" = "ParameterTwo description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterOne" = '[string]'
+                    "ParameterTwo" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Not -Throw
+
+        }
+
+        It "should throw when multiple param block variables do not match .PARAMETER help when one is invalid" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterA'
+                            "LineNumber" = 1
+                            "Text" = "ParameterA description"
+                        },
+                        @{
+                            "Name" = 'ParameterTwo'
+                            "LineNumber" = 1
+                            "Text" = "ParameterTwo description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterOne" = '[string]'
+                    "ParameterTwo" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when multiple param block variables do not match .PARAMETER help when all are invalid" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterA'
+                            "LineNumber" = 1
+                            "Text" = "ParameterA description"
+                        },
+                        @{
+                            "Name" = 'ParameterB'
+                            "LineNumber" = 1
+                            "Text" = "ParameterB description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterOne" = '[string]'
+                    "ParameterTwo" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when multiple param block variables do not match .PARAMETER help when one is missing" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterOne'
+                            "LineNumber" = 1
+                            "Text" = "ParameterOne description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterOne" = '[string]'
+                    "ParameterTwo" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when multiple param block variables do not match .PARAMETER help when all are missing" {
+
+            {
+                $help = @{
+                    '.SYNOPSIS' = @(
+                        @{
+                            "Name" = ''
+                            "LineNumber" = 1
+                            "Text" = "Description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterOne" = '[string]'
+                    "ParameterTwo" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when .PARAMETER help does not match param block variables when one is missing" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterOne'
+                            "LineNumber" = 1
+                            "Text" = "ParameterOne description"
+                        },
+                        @{
+                            "Name" = 'ParameterTwo'
+                            "LineNumber" = 1
+                            "Text" = "ParameterTwo description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                    "ParameterTwo" = '[string]'
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
+        It "should throw when .PARAMETER help does not match param block variables when all are missing" {
+
+            {
+                $help = @{
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = 'ParameterOne'
+                            "LineNumber" = 1
+                            "Text" = "ParameterOne description"
+                        },
+                        @{
+                            "Name" = 'ParameterTwo'
+                            "LineNumber" = 1
+                            "Text" = "ParameterTwo description"
+                        }
+                    )
+                }
+                $parameterVariable = @{
+                }
+
+                Test-HelpTokensParamsMatch -HelpTokens $help -ParameterVariables $parameterVariable
+
+            } | Should -Throw
+
+        }
+
     }
 
 }
