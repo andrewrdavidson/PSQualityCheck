ddba502138d5d41e6c9bdc87e5ed8ea34ad69d84
Andrew Davidson <github@kelrys.com>
2021-01-09 21:21:25 +0000

    Refactored code to be more useful

--- Source/PSQualityCheck.Functions/Test-HelpForRequiredTokens.ps1
+++ Source/PSQualityCheck.Functions/Test-HelpForRequiredTokens.ps1
@@ -23,9 +23,11 @@ function Test-HelpForRequiredTokens {
 
         $module = Get-Module -Name PSQualityCheck
 
-        if (Test-Path -Path (Join-Path -Path $module.ModuleBase -ChildPath "Checks\HelpElementRules.psd1")) {
+        $helpElementRulesPath = (Join-Path -Path $module.ModuleBase -ChildPath "Checks\HelpElementRules.psd1")
 
-            $helpElementRules = (Import-PowerShellDataFile -Path (Join-Path -Path $module.ModuleBase -ChildPath "Checks\HelpElementRules.psd1"))
+        if (Test-Path -Path $helpElementRulesPath) {
+
+            $helpElementRules = Import-PowerShellDataFile -Path $helpElementRulesPath
 
         }
         else {
