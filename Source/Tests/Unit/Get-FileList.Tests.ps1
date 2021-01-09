e986957174431ca20654c03717c9acdab095ea4f
Andrew Davidson <github@kelrys.com>
2021-01-09 16:39:38 +0000

    Added Get-FileList test

--- Source/Tests/Unit/Get-FileList.Tests.ps1
+++ Source/Tests/Unit/Get-FileList.Tests.ps1
@@ -44,6 +44,116 @@ Describe "Get-FileList.Tests" {
 
         }
 
+        It "should return one file when checking folder with one matching files" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test1.ps1'
+            Set-Content -Path $testPath1 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+
+            $fileList.Count | Should -BeExactly 1
+
+        }
+
+        It "should return two files when checking folder with two matching files" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test1.ps1'
+            Set-Content -Path $testPath1 -Value $fileContent
+            $testPath2 = Join-Path -Path $TestDrive -ChildPath 'test2.ps1'
+            Set-Content -Path $testPath2 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+            Remove-Item -Path $testPath2 -Force
+
+            $fileList.Count | Should -BeExactly 2
+
+        }
+
+        It "should return no files when checking folder with non-matching files" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test1.psm1'
+            Set-Content -Path $testPath1 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+
+            $fileList.Count | Should -BeExactly 0
+
+        }
+
+        It "should return one file when checking folder with some matching files" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test1.psm1'
+            Set-Content -Path $testPath1 -Value $fileContent
+            $testPath2 = Join-Path -Path $TestDrive -ChildPath 'test2.ps1'
+            Set-Content -Path $testPath2 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+            Remove-Item -Path $testPath2 -Force
+
+            $fileList.Count | Should -BeExactly 1
+
+        }
+
+        It "should return correct file when checking folder" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test1.psm1'
+            Set-Content -Path $testPath1 -Value $fileContent
+            $testPath2 = Join-Path -Path $TestDrive -ChildPath 'test2.ps1'
+            Set-Content -Path $testPath2 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+            Remove-Item -Path $testPath2 -Force
+
+            $fileList | Should -BeExactly $testPath2
+
+        }
+
+        It "should return multiple correct files when checking folder" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test1.ps1'
+            Set-Content -Path $testPath1 -Value $fileContent
+            $testPath2 = Join-Path -Path $TestDrive -ChildPath 'test2.ps1'
+            Set-Content -Path $testPath2 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+            Remove-Item -Path $testPath2 -Force
+
+            $fileList | Should -BeExactly  @($testPath1, $testPath2)
+
+        }
+
+        It "should not return Test files when checking folder" {
+
+            $fileContent = "function Get-FileContent {}"
+            $testPath1 = Join-Path -Path $TestDrive -ChildPath 'test.Tests.ps1'
+            Set-Content -Path $testPath1 -Value $fileContent
+
+            $fileList = Get-FileList -Path $TestDrive -Extension ".ps1"
+
+            Remove-Item -Path $testPath1 -Force
+
+            $fileList.Count | Should -BeExactly 0
+
+        }
+
     }
 
 }
