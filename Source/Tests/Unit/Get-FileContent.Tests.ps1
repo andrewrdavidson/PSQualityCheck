33a8db3e273c5059fffe46d9791a195a9d71bde9
Andrew Davidson <github@kelrys.com>
2021-01-09 15:47:58 +0000

    Added Get-FileContentTest and fixes to function

--- Source/Tests/Unit/Get-FileContent.Tests.ps1
+++ Source/Tests/Unit/Get-FileContent.Tests.ps1
@@ -37,6 +37,133 @@ Describe "Get-FileContent.Tests" {
 
         }
 
+        It "should pass when Path is valid with no function, empty content in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent = ""
+            Set-Content -Path $testPath -Value $fileContent
+
+            $parsedFileContent = Get-FileContent -Path $testPath
+
+            ($fileContent -eq $parsedFileContent) | Should -BeTrue
+
+        }
+
+        It "should pass when Path is valid with no function, single-line content in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent = "Write-Host"
+            Set-Content -Path $testPath -Value $fileContent
+
+            $parsedFileContent = Get-FileContent -Path $testPath
+
+            ($fileContent -eq $parsedFileContent) | Should -BeTrue
+
+        }
+
+        It "should pass when Path is valid with no function, multi-line content in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent = "Write-Host
+            Write-Host
+            "
+            Set-Content -Path $testPath -Value $fileContent
+
+            $parsedFileContent = Get-FileContent -Path $testPath
+
+            ($fileContent -eq $parsedFileContent) | Should -BeTrue
+
+        }
+
+        It "should pass when Path is valid with only one empty function in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent = "function Get-FileContent {}"
+            $matchContent = ""
+            Set-Content -Path $testPath -Value $fileContent
+
+            $parsedFileContent = Get-FileContent -Path $testPath
+
+            ($matchContent -eq $parsedFileContent) | Should -BeTrue
+
+        }
+
+        It "should pass when Path is valid with only one single-line function in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent = "function Get-FileContent { Write-Host }"
+            $matchContent = " Write-Host "
+            Set-Content -Path $testPath -Value $fileContent
+
+            $parsedFileContent = Get-FileContent -Path $testPath
+
+            ($matchContent -eq $parsedFileContent) | Should -BeTrue
+
+        }
+
+        It "should pass when Path is valid with only one single-line advanced function in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent = "function Get-FileContent { if ($true) { Write-Host } }"
+            $matchContent = " if ($true) { Write-Host } "
+            Set-Content -Path $testPath -Value $fileContent
+
+            $parsedFileContent = Get-FileContent -Path $testPath
+
+            ($matchContent -eq $parsedFileContent) | Should -BeTrue
+
+        }
+
+        It "should pass when Path is valid with only one multi-line function in the file" {
+
+            $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+            $fileContent1 = "function Get-FileContent {
+                                Write-Host
+                            }"
+            $matchContent1 = "                                Write-Host`r`n"
+
+            Set-Content -Path $testPath -Value $fileContent1
+
+            $parsedFileContent1 = Get-FileContent -Path $testPath
+
+            $fileContent2 = "function Get-FileContent {
+                                if ($true) {
+                                    Write-Host
+                                }
+                            }"
+            $matchContent2 = "                                if ($true) {
+                                    Write-Host
+                                }`r`n"
+
+            Set-Content -Path $testPath -Value $fileContent2
+
+            $parsedFileContent2 = Get-FileContent -Path $testPath
+
+            ($matchContent1 -eq $parsedFileContent1) | Should -BeTrue
+            ($matchContent2 -eq $parsedFileContent2) | Should -BeTrue
+
+        }
+
+        It "should throw when Path is valid with two functions in the file" {
+
+            {
+                $testPath = Join-Path -Path $TestDrive -ChildPath 'test.txt'
+                $fileContent = "function Get-FileContent {
+                                    Write-Host
+                                }
+
+                                function Test-Function {
+                                    Write-Host
+                                }"
+
+                Set-Content -Path $testPath -Value $fileContent
+
+                $parsedFileContent = Get-FileContent -Path $testPath
+
+            } | Should -Throw
+
+        }
+
     }
 
 }
