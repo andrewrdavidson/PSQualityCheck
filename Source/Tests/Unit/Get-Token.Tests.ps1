c2785a684ff21873a2f472e122dd44363ce04824
Andrew Davidson <github@kelrys.com>
2021-01-09 20:13:44 +0000

    Added Get-Token tests

--- Source/Tests/Unit/Get-Token.Tests.ps1
+++ Source/Tests/Unit/Get-Token.Tests.ps1
@@ -53,4 +53,82 @@ Describe "Get-Token.Tests" {
 
     }
 
+
+    BeforeAll {
+        $parsedFileContent = @(
+            @{
+                "Content" = "function"
+                "Type" = "Keyword"
+                "Start" = 0
+                "Length" = 8
+                "StartLine" = 1
+                "StartColumn" = 1
+                "EndLine" = 1
+                "EndColumn" = 9
+            },
+            @{
+                "Content" = "Get-FileContent"
+                "Type" = "CommandArgument"
+                "Start" = 9
+                "Length" = 15
+                "StartLine" = 1
+                "StartColumn" = 10
+                "EndLine" = 1
+                "EndColumn" = 25
+            },
+            @{
+                "Content" = "{"
+                "Type" = "GroupStart"
+                "Start" = 25
+                "Length" = 1
+                "StartLine" = 1
+                "StartColumn" = 26
+                "EndLine" = 1
+                "EndColumn" = 27
+            },
+            @{
+                "Content" = "}"
+                "Type" = "GroupEnd"
+                "Start" = 26
+                "Length" = 1
+                "StartLine" = 1
+                "StartColumn" = 27
+                "EndLine" = 1
+                "EndColumn" = 28
+            }
+        )
+    }
+
+    It "should find token where parameters are valid" {
+
+        $token = Get-Token -ParsedFileContent $parsedFileContent -Type "Keyword" -Content "Function"
+
+        Write-Host ($token | Out-String) -ForegroundColor Cyan
+        Write-Host $tokenMatch.StartLine -ForegroundColor Magenta
+
+        for ($x = 0; $x -lt $parsedModule.Count; $x++) {
+
+            (
+                ($token[$x].StartLine -eq $parsedFileContent[$x].StartLine) -and
+                ($token[$x].Content -eq $parsedFileContent[$x].Content) -and
+                ($token[$x].Type -eq $parsedFileContent[$x].Type) -and
+                ($token[$x].Start -eq $parsedFileContent[$x].Start) -and
+                ($token[$x].Length -eq $parsedFileContent[$x].Length) -and
+                ($token[$x].StartColumn -eq $parsedFileContent[$x].StartColumn) -and
+                ($token[$x].EndLine -eq $parsedFileContent[$x].EndLine) -and
+                ($token[$x].EndColumn -eq $parsedFileContent[$x].EndColumn)
+            ) | Should -BeTrue
+
+        }
+
+    }
+
+    It "should not find token where parameters are invalid" {
+
+        $token = (Get-Token -ParsedFileContent $parsedFileContent -Type "Unknown" -Content "Data")
+
+        $token | Should -BeNullOrEmpty
+
+    }
+
 }
