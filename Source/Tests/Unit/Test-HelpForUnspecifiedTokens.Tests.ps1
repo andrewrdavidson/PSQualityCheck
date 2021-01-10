68bdc9e4bc01230d7f7128d7b429a1535f9c2486
Andrew Davidson <github@kelrys.com>
2021-01-10 12:37:46 +0000

    Added Test-HelpForUnspecifiedTokens tests

--- Source/Tests/Unit/Test-HelpForUnspecifiedTokens.Tests.ps1
+++ Source/Tests/Unit/Test-HelpForUnspecifiedTokens.Tests.ps1
@@ -27,11 +27,187 @@ Describe "Test-HelpForUnspecifiedTokens.Tests" {
 
     Context "Function tests" {
 
+        BeforeAll {
+            New-Item -Path (Join-Path -Path $TestDrive -ChildPath 'module') -ItemType Directory
+            New-Item -Path (Join-Path -Path $TestDrive -ChildPath 'module\checks') -ItemType Directory
+
+            '@{
+                ''1'' = @{
+                    Key = ''.SYNOPSIS''
+                    Required = $true
+                    MinOccurrences = 1
+                    MaxOccurrences = 1
+                }
+                ''2'' = @{
+                    Key = ''.DESCRIPTION''
+                    Required = $true
+                    MinOccurrences = 1
+                    MaxOccurrences = 1
+                }
+                ''3'' = @{
+                    Key = ''.PARAMETER''
+                    Required = $false
+                    MinOccurrences = 0
+                    MaxOccurrences = 0
+                }
+                ''4'' = @{
+                    Key = ''.EXAMPLE''
+                    Required = $true
+                    MinOccurrences = 1
+                    MaxOccurrences = 100
+                }
+                ''5'' = @{
+                    Key = ''.INPUTS''
+                    Required = $false
+                    MinOccurrences = 0
+                    MaxOccurrences = 0
+                }
+                ''6'' = @{
+                    Key = ''.OUTPUTS''
+                    Required = $false
+                    MinOccurrences = 0
+                    MaxOccurrences = 0
+                }
+                ''7'' = @{
+                    Key = ''.NOTES''
+                    Required = $false
+                    MinOccurrences = 0
+                    MaxOccurrences = 0
+                }
+                ''8'' = @{
+                    Key = ''.LINK''
+                    Required = $false
+                    MinOccurrences = 0
+                    MaxOccurrences = 0
+                }
+            }' | Set-Content -Path (Join-Path -Path $TestDrive -ChildPath 'module\checks\HelpElementRules.psd1')
+        }
+
+        BeforeEach {
+            Mock Get-Module -ParameterFilter { $Name -eq "PSQualityCheck" } {
+                return @{'ModuleBase' = Join-Path -Path $TestDrive -ChildPath 'module' }
+            }
+        }
+
         It "should throw when passing null parameters" {
 
             {
 
                 Test-HelpForUnspecifiedTokens -HelpTokens $null
+
+            } | Should -Throw
+
+        }
+
+        It "should not throw when checking required help tokens" {
+
+            {
+                $helpTokens = @{
+                    '.SYNOPSIS' = @(
+                        @{
+                            "Name" = $null
+                            "LineNumber" = 1
+                            "Text" = ""
+                        }
+                    )
+                    '.DESCRIPTION' = @(
+                        @{
+                            "Name" = $null
+                            "LineNumber" = 3
+                            "Text" = ""
+                        }
+                    )
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = "Path"
+                            "LineNumber" = 5
+                            "Text" = ""
+                        }
+                    )
+                    '.EXAMPLE' = @(
+                        @{
+                            "Name" = ""
+                            "LineNumber" = 7
+                            "Text" = "This is example text"
+                        }
+                    )
+                }
+
+                Test-HelpForRequiredTokens -HelpTokens $helpTokens
+
+                Assert-MockCalled -CommandName Get-Module -Times 1 -ParameterFilter { $Name -eq "PSQualityCheck" }
+
+            } | Should -Not -Throw
+
+        }
+
+        It "should not throw when checking required help tokens plus optional help tokens" {
+
+            {
+
+                $helpTokens = @{
+                    '.SYNOPSIS' = @(
+                        @{
+                            "Name" = $null
+                            "LineNumber" = 1
+                            "Text" = ""
+                        }
+                    )
+                    '.DESCRIPTION' = @(
+                        @{
+                            "Name" = $null
+                            "LineNumber" = 3
+                            "Text" = ""
+                        }
+                    )
+                    '.PARAMETER' = @(
+                        @{
+                            "Name" = "Path"
+                            "LineNumber" = 5
+                            "Text" = ""
+                        }
+                    )
+                    '.EXAMPLE' = @(
+                        @{
+                            "Name" = ""
+                            "LineNumber" = 7
+                            "Text" = "This is example text"
+                        }
+                    )
+                    '.NOTES' = @(
+                        @{
+                            "Name" = ""
+                            "LineNumber" = 10
+                            "Text" = "This is a note"
+                        }
+                    )
+
+                }
+
+                Test-HelpForRequiredTokens -HelpTokens $helpTokens
+
+                Assert-MockCalled -CommandName Get-Module -Times 1 -ParameterFilter { $Name -eq "PSQualityCheck" }
+
+            } | Should -Not -Throw
+
+        }
+
+        It "should throw when unspecified help token is present" {
+
+            {
+                $helpTokens = @{
+                    '.DUMMY' = @(
+                        @{
+                            "Name" = $null
+                            "LineNumber" = 1
+                            "Text" = ""
+                        }
+                    )
+                }
+
+                Test-HelpForRequiredTokens -HelpTokens $helpTokens
+
+                Assert-MockCalled -CommandName Get-Module -Times 1 -ParameterFilter { $Name -eq "PSQualityCheck" }
 
             } | Should -Throw
 
