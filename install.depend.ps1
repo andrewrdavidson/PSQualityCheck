080e6e7feeffa805998ade191690c1942a92931f
Andrew Davidson <github@kelrys.com>
2024-01-18 10:06:06 +0000

    Added requirement for running as administrtor; code reformatting

--- install.depend.ps1
+++ install.depend.ps1
@@ -1,37 +1,38 @@
-Write-Output "Checking for nuget.exe"
+#requires -RunAsAdministrator
+
+Write-Output 'Checking for nuget.exe'
 if (-not ($NuGetPath = (Get-Command 'nuget.exe' -ErrorAction SilentlyContinue).Path)) {
 
-    if (-not (Test-Path -Path ".\bin" -ErrorAction SilentlyContinue)) {
-        New-Item -Path "bin" -ItemType Directory
+    if (-not (Test-Path -Path '.\bin' -ErrorAction SilentlyContinue)) {
+        New-Item -Path 'bin' -ItemType Directory
     }
 
-    Write-Output "nuget.exe not found, downloading into project bin path"
-    $NuGetPath = Resolve-Path -Path ".\bin"
-    $NuGetFile = Join-Path -Path $NuGetPath -ChildPath "nuget.exe"
+    Write-Output 'nuget.exe not found, downloading into project bin path'
+    $NuGetPath = Resolve-Path -Path '.\bin'
+    $NuGetFile = Join-Path -Path $NuGetPath -ChildPath 'nuget.exe'
     Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $NuGetFile
 
     $path = [Environment]::GetEnvironmentVariable('Path', 'Machine')
-    $environmentPaths = $path.Split(";")
+    $environmentPaths = $path.Split(';')
     if (-not ($NuGetPath -contains $environmentPaths)) {
-        Write-Output "Updating System Environment path"
+        Write-Output 'Updating System Environment path'
         $path += ";$NuGetPath"
         $env:Path = $path
-        [Environment]::SetEnvironmentVariable("Path", $path, 'Machine')
+        [Environment]::SetEnvironmentVariable('Path', $path, 'Machine')
     }
 
 }
 
 # Bootstrap environment
-Write-Output "Bootstrap NuGet PackageProvider"
+Write-Output 'Bootstrap NuGet PackageProvider'
 Get-PackageProvider -Name 'NuGet' -ForceBootstrap | Out-Null
 
 # Install PSDepend module if it is not already installed
 if (-not (Get-Module -Name 'PSDepend' -ListAvailable)) {
-    Write-Output "PSDepend is not yet installed, installing PSDepend"
+    Write-Output 'PSDepend is not yet installed, installing PSDepend'
     Install-Module -Name 'PSDepend' -Scope 'CurrentUser' -Force
-}
-else {
-    Write-Output "PSDepend already installed"
+} else {
+    Write-Output 'PSDepend already installed'
 }
 
 # Install build dependencies
@@ -47,4 +48,4 @@ $invokePSDependParams = @{
 }
 Invoke-PSDepend @invokePSDependParams
 
-Write-Output "Finished installing all dependencies"
+Write-Output 'Finished installing all dependencies'
